612368c6323930e6a86cf6e846011807b5bdb776
laynaria <pro.laynaria@outlook.fr>
2024-05-13 09:42:43 +0200

    graphql setup in frontend + test query in index.tsx + Types creations for Countries and Continents

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,42 @@
+import { gql, useQuery } from "@apollo/client";
+import { useState } from "react";
+
+const GET_ALL_COUNTRIES = gql`
+  query Countries {
+    countries {
+      id
+      code
+      name
+      emoji
+      continent {
+        id
+        name
+      }
+    }
+  }
+`;
+
 export default function Home() {
-  return <h1>Hello, wilder !</h1>;
+  const [countries, setCountries] = useState<Country[]>([]);
+
+  const { loading, error } = useQuery(GET_ALL_COUNTRIES, {
+    onCompleted: (data: any) => {
+      console.log(data.countries);
+      setCountries(data.countries);
+    },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
+
+  return (
+    <main>
+      <section>
+        <h1>Hello, wilder !</h1>
+        {countries?.map((country) => (
+          <h2 key={country.id}>{country.name}</h2>
+        ))}
+      </section>
+    </main>
+  );
 }
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,1 +1,13 @@
 // TODO
+type Continent = {
+  id: number;
+  name: string;
+};
+
+type Country = {
+  id: number;
+  name: string;
+  code: string;
+  emoji: string;
+  continent: Continent;
+};
